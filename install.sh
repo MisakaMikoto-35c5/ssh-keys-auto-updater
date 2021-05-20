691c87361cef2fac8dacb465616929c6e9970608
MisakaMikoto-35c5 <github-contact-public@breakwall.xyz>
2021-05-21 01:50:07 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,9 +10,9 @@ touch $INSTALL_DIR/github_authorized_keys
 touch $INSTALL_DIR/authorized_keys_base
 touch $INSTALL_DIR/authorized_keys
 
-chmod 600 $SSH_DIR/github_authorized_keys
-chmod 600 $SSH_DIR/authorized_keys_base
-chmod 644 $SSH_DIR/authorized_keys
+chmod 600 $INSTALL_DIR/github_authorized_keys
+chmod 600 $INSTALL_DIR/authorized_keys_base
+chmod 644 $INSTALL_DIR/authorized_keys
 
 wget -O ~/.ssh/update-keys.sh $SCRIPT_RAW_URL_PREFIX/update-keys.sh
 chmod 600 ~/.ssh/update-keys.sh
